5da1efad94e0f319cfe5eda84dfee37e4baf31eb
Lucas Calçada <lucasmgmcalcada@gmail.com>
2023-12-04 18:51:24 -0300

    Made all variables in constants.hpp constant

--- constants.hpp
+++ constants.hpp
@@ -1,13 +1,17 @@
 #include "Arduino.h"
+
 #pragma once
+const uint8_t ledPins[] = {4,5,6,7};
 
-int tones[] = {523,587,659,698};
-uint8_t ledPins[] = {4,5,6,7};
-
-// Note Player
-uint8_t tonePin = 0;
+// ---------- Note Player ----------
+const int tones[] = {523,587,659,698};
+const uint8_t tonePin = 0;
 // Note length in ms
-int toneLength = 500;
+const int toneLength = 500;
 // Note sequence feedback
-int badNoteSequence[] = {};
-int goodNoteSequence[] = {};
+const int badNoteSequence[] = {};
+const int goodNoteSequence[] = {};
+
+// ---------- Input Listener --------
+const uint8_t btnPowerPin = 0;
+const uint8_t btnPins[] = {24,25,26,27};
